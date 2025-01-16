5871c7cff417ea53ae30305b530668217380cd59
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-01-16 16:01:55 +0100

    getters & setters

--- src/entities/Personne.java
+++ src/entities/Personne.java
@@ -6,14 +6,42 @@ public class Personne {
     private AdressePostale adressePostale;
 
     public Personne(String nom, String prenom, AdressePostale adressePostale) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.adressePostale = adressePostale;
+        this.setNom(nom);
+        this.setNom(prenom);
+        this.setAdressePostale(adressePostale);
     }
 
     public Personne(String nom, String prenom) {
+        this.setNom(nom);
+       this.setNom(prenom);
+    }
+
+    public void displayNames()   {
+        System.out.println("Nom : " + this.nom.toUpperCase() + " Prénom : " + this.prenom);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
         this.prenom = prenom;
+    }
+
+    public AdressePostale getAdressePostale() {
+        return adressePostale;
+    }
+
+    public void setAdressePostale(AdressePostale adressePostale) {
+        this.adressePostale = adressePostale;
     }
 }
 
