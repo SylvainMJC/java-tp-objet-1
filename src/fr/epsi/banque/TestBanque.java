ed7c71a58942b335f1e7791b160a7ac07ba981ad
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-01-16 16:17:52 +0100

    tp3

---
+++ src/fr/epsi/banque/TestBanque.java
@@ -0,0 +1,12 @@
+package fr.epsi.banque;
+
+import fr.epsi.banque.entites.Compte;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        Compte compte = new Compte("123456789", 1000.50);
+        System.out.println(compte);
+        // c'est l'adresse mémoire de l'objet qui est affichée
+    }
+
+}
---
+++ src/fr/epsi/banque/entites/Compte.java
@@ -0,0 +1,11 @@
+package fr.epsi.banque.entites;
+
+public class Compte {
+    private String numeroCompte;
+    private double solde;
+
+    public Compte(String numeroCompte, double solde) {
+        this.numeroCompte = numeroCompte;
+        this.solde = solde;
+    }
+}
